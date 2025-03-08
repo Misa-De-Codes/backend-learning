9237a1b2478a5c84e72a54166ca489f1812c53cf
Musa <musaddikulislam007@gmail.com>
2025-03-08 21:16:37 +0530

    lacture 8 utils code dissussion

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
 
+
 dotenv.config({
     path: './env'
 })
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 5500, () => {
+        console,log(` server is running at port: ${process.env.PORT}`)
+    })
 
-
+})
+.catch((err) => {
+    console.log("MONNGO db connection failed !!! ", err)
+})
 
 
 
